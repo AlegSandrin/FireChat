a0ffdc9f6a372d422ef8b3c90a3c684d40addb29
Alexsandro Urbano <99767577+AlegSandrin@users.noreply.github.com>
2023-03-03 20:10:46 -0300

    design base do chat

--- src/Home.js
+++ src/Home.js
@@ -9,6 +9,9 @@ import { useCollectionData } from 'react-firebase-hooks/firestore'
 
 import photoURL from './assets/img/avatar.png'
 
+import { FaSignOutAlt } from 'react-icons/fa'
+import { MdContactMail } from 'react-icons/md'
+
 firebase.initializeApp({
   apiKey: "AIzaSyC1Jv-EHjRSlq7jNGMKXO6P3QVyIWrf5Xc",
   authDomain: "mychat-155c4.firebaseapp.com",
@@ -26,14 +29,21 @@ export default function Home() {
     const [user] = useAuthState(auth);
 
     return(
-        <div className='chat'>
-            <header>
-
-            </header>
-
-            <section>
-                {user ? <ChatRoom /> : <SignIn />}
-            </section>
+        <div className='Chat w-[100wh] h-[100vh] p-12'>
+            <div className='bg-slate-800 text-white rounded-3xl grid grid-cols-12 grid-rows-8 mx-auto my-auto h-full w-full overflow-hidden drop-shadow-2xl shadow-inner'>
+                <header className='row-span-1 col-span-3 p-4 color2 overflow-hidden'>
+                    <SignOut/>
+                </header>
+                <div className='col-span-3 row-start-2 row-end-7 overflow-y-auto color1'>
+                <div className='flex gap-1 text-xl ml-4 mt-3'>
+                    <MdContactMail/>
+                    <h1 className=''>Contatos</h1>
+                    </div>
+                </div>
+                <section className='col-span-9 row-span-6 col-start-4 overflow-hidden color4'>
+                    {user ? <ChatRoom /> : <SignIn />}
+                </section>
+            </div>
         </div>
     )
 }
@@ -52,7 +62,12 @@ function SignIn() {
 
 function SignOut() {
     return auth.currentUser && (
-        <button onClick={() => auth.signOut()}>Sign Out</button>
+        <button onClick={() => auth.signOut()}>
+            <div className='flex gap-1 text-xl'>
+            <FaSignOutAlt/>
+            <span className='text-base'>Sair</span>
+            </div>
+            </button>
     )
 }
 
@@ -86,19 +101,21 @@ function ChatRoom() {
 
     return(
         <>
-        <div className='border border-black w-[500px] h-[80vh] mx-auto my-auto'>
-            <main >
+        <div className='flex flex-col place-content-end h-full w-full'>
+            <main className='place-content-start h-full overflow-y-auto'>
                 { messages && messages.map(msg => <ChatMessage key={msg.id} message={msg}/>) }
 
                 <div ref={ScrollToEnd}></div>
 
             </main>
 
-            <form onSubmit={sendMessage}>
+            <form className='flex m-4 rounded-xl overflow-hidden' onSubmit={sendMessage}>
 
-                <input value={formValue} onChange={(e) => setFormValue(e.target.value)}/>
+                <input
+                className='w-full h-full p-4 overscroll-contain text-black outline-0'
+                value={formValue} onChange={(e) => setFormValue(e.target.value)}/>
 
-                <button type="submit">Enviar</button>
+                <button className='border-l-2 p-2 px-5 float-right color5 border-none' type="submit">Enviar</button>
 
             </form>
         </div>
@@ -113,10 +130,11 @@ function ChatMessage(props) {
     const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
 
     return (
-        <div className={`message ${messageClass}`}>
-            <img className='h-[50px] w-auto' src={photoURL}/>
-            <p>{text}</p>
+        <div className={`message ${messageClass} flex m-5 rounded-xl color4 brightness-125`}>
+            <img className='h-[40px] rounded-full m-2 w-auto' src={photoURL}/>
+            <p className='m-2 font-extralight text-sm'>{text}</p>
 
         </div>
     )
 }
+
