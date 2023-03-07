a276966e8b90a3e9d05401664e7000c6644132a7
Alexsandro Urbano <99767577+AlegSandrin@users.noreply.github.com>
2023-03-07 20:55:11 -0300

    desenvolvendo o chat privado

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { useAuthState } from "react-firebase-hooks/auth";
 import { Outlet } from "react-router-dom";
-import initializeFireBase from "./services/firebaseService";
+import Loading from "./components/Loading";
+import SignIn from "./pages/SignIn";
+import {auth} from "./services/firebaseService";
 
-initializeFireBase()
 
 function App() {
+
+  const [user, loading] = useAuthState(auth)
+
+  if (loading) return <Loading/>
+
+  if (!user) return <SignIn/>
+
   return (
     <div className="App h-full w-full">
       <Outlet/>
--- src/services/firebaseService.js
+++ src/services/firebaseService.js
@@ -1,12 +1,20 @@
 import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth'
+import 'firebase/compat/firestore'
 
-export default function initalizeFirebase(){
-firebase.initializeApp({
+const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_FIREBASE_APP_ID
-  })
-}
+  }
+
+const app = firebase.initializeApp(firebaseConfig)
+
+const db = app.firestore()
+const auth = app.auth()
+const provider = new firebase.auth.GoogleAuthProvider()
+
+export { db, auth, provider }
