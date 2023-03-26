573c419c292ab35efad7f8fc292fa1aedb03a464
Alexsandro Urbano <99767577+AlegSandrin@users.noreply.github.com>
2023-03-26 10:46:21 -0300

    *

--- public/electron.js
+++ public/electron.js
@@ -1,27 +1,34 @@
-const { app, BrowserWindow } = require("electron")
-const path = require("path")
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+const isDev = require("electron-is-dev");
 
-const isDev = require("electron-is-dev")
-
-function createWindow(){
-    const win = new BrowserWindow({
-        width: 1280,
-        height: 720,
-        webPreferences: {
-            preload: path.join(__dirname, "preloader.js")
-        }
-    })
-    win.loadURL(isDev ? "http:localhost:3000" : `file://${path.join(__dirname, "../build/index.html")}`)
+function createWindow() {
+  const win = new BrowserWindow({
+    title: "FireChat",
+    webPreferences: {
+      preload: path.join(__dirname, "preloader.js"),
+    },
+  });
+  win.maximize();
+  win.setMenu(null);
+  win.loadURL(
+    isDev
+      ? "http:localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  );
+  win.on("page-title-updated", function (e) {
+    e.preventDefault();
+  });
 }
 
 app.whenReady().then(() => {
-    createWindow()
+  createWindow();
 
-    app.on("active", () => {
-        if(BrowserWindow.getAllWindows().length === 0) createWindow()
-    })
-})
+  app.on("active", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
 
 app.on("window-all-closed", () => {
-    if (process.platform !== "darwin") app.quit()
-})
+  if (process.platform !== "darwin") app.quit();
+});
